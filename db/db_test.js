6fdcb01bb57398596c949c49960b65761688a351
SleepyOliveTrees <olismi27@bergen.org>
2024-12-17 14:11:34 -0500

    Updated .env (maybe work)

--- db/db_test.js
+++ db/db_test.js
@@ -1,7 +1,7 @@
 // Import the connection from db_connection.js
 const db = require("./db_connection");
 
-db.execute("SELECT * FROM subjects", (error, results) => {
+db.execute("SELECT * FROM user", (error, results) => {
   if (error) {
     throw error;
   } else {
