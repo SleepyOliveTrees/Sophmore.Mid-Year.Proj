12b5106cf983cb0de5291475a97a79e1cba8de3b
avnkau27 <avnkau27@bergen.org>
2024-12-18 00:15:52 -0500

    almost done with login/signup feature

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+//home page
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click', ()=> {
+    wrapper.classList.add('active')
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active')
+});
+
 // task list
 const express = require("express");
 const app = express();
