9f0dfb77941659a295ace68b3f905d12c2c0a1d0
SleepyOliveTrees <olismi27@bergen.org>
2024-12-23 01:56:07 -0500

    Database might finally be connected
    Had to add some libraries(? I think that's what they're called): dotemv, ejs, mysql2, and sophmore.mid-year.proj
    Had another db testing file that works, but original doesn't
    Mostly updated: db folder, moved .env back (will gitignore later), notes, package-lock.json, and package.json
    Added .gitignore cuz of errors

--- db/db_test_2.js
+++ db/db_test_2.js
@@ -3,16 +3,23 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-// Establish a connection with the database
 const dbConfig = {
   host: process.env.DB_HOST || "localhost",
   port: parseInt(process.env.DB_PORT || "3306"),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  connectTimeout: parseInt(process.env.DB_CONNECT_TIMEOUT),
+  connectTimeout: parseInt(process.env.DB_CONNECT_TIMEOUT || "10000"),
 };
 
 const connection = mysql.createConnection(dbConfig);
 
-module.exports = connection;
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err.message);
+  } else {
+    console.log("Connected to the database.");
+  }
+});
+
+connection.end();
